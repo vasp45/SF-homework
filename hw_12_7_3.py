a387490da15ef51d48185cf7bd2e82778522ca1c
vasp45 <12879336+vasp45@users.noreply.github.com>
2023-01-25 07:07:02 +0300

    Update hw_12_7_3.py

--- hw_12_7_3.py
+++ hw_12_7_3.py
@@ -4,13 +4,15 @@
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 
 while True:
-	try:
-		money = int(input('Введите сумму вклада: '))
-		break
-	except ValueError:
-		print('Введенное значение должно быть числом.\n')
+    try:
+        money = int(input('Введите сумму вклада: '))
+        break
+    except ValueError:
+        print('Введенное значение должно быть числом.\n')
 
-deposit = [money/100*v for v in per_cent.values()]
+deposit = {key:money/100*v for key, v in per_cent.items()}
 
-print ('\nМаксимальная сумма, которую вы можете заработать: %.2f'%(max(deposit)))
+print ( '\nМаксимальная сумма, которую вы можете заработать: '+
+        '{0[1]:.2f}, банк: {0[0]:s}'.format(
+        (max(deposit.items(), key=lambda v: v[1]))))
 

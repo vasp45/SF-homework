d3761c10a34411114618dcb5168a28fd0f586a67
vasp45 <12879336+vasp45@users.noreply.github.com>
2023-01-27 19:31:40 +0300

    Update hw_13.8.19.py

--- hw_13.8.19.py
+++ hw_13.8.19.py
@@ -19,19 +19,19 @@ def my_input(text):
 tickets = my_input('Введите количество билетов, которое хотите приобрести: ')
       
 
-print ( '\nВы хотите приобрести %d билет%s' % (tickets, 
-        '' if str(tickets)[-1] == '1' and tickets != 11 else (
-        'a' if str(tickets)[-1] in '234' else 'ов'))+'\n')
+print('\nВы хотите приобрести %d билет%s\n' % ( tickets,
+                                                '' if str(tickets)[-1] == '1' and tickets != 11 else (
+                                                'a' if str(tickets)[-1] in '234' else 'ов')))
 
 
 price = 0
 for t in range(1, tickets+1):
     age = my_input(f'Введите возраст {t}-го посетителя: ')
     
-    if 18 <= age <= 25: 
+    if 18 <= age < 25: 
         price += 990
-    elif age > 25: 
+    elif age >= 25: 
         price += 1390
     
-print ( '\nИтого:\n  количество билетов: %d\n  общая стоимость: %d руб.\n  %s'%(
+print(  '\nИтого:\n  количество билетов: %d\n  общая стоимость: %d руб.\n  %s'%(
         tickets, price*0.9 if tickets > 3 else price, 'Скидка 10% !' if price and tickets>3 else '' ))
